f099cb8c03a537f3d0fdad75e47273873ffa6829
sandeepvarma8466 <svarma852@gmail.com>
2022-07-13 21:31:32 +0530

    welcome to regex pincode problem

---
+++ src/main/java/com/blz/regex/pincode/PinCode.java
@@ -0,0 +1,39 @@
+package com.blz.regex.pincode;
+
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+public class PinCode {
+    public static final Scanner sc = new Scanner(System.in);
+    public static final String PINCODE = "^[0-9]{6}$";
+    public static void main(String[] args) {
+        PinCode pinCode = new PinCode();
+        System.out.println("Enter pincode : ");
+        pinCode.isValidPostCode(sc.nextLine());
+    }
+
+    public void isValidPostCode(String name){
+        try {
+            if (isValidInput(PINCODE,name)){
+                System.out.println("Valid PinCode is : " +name);
+            }else {
+                throw new InvalidUserInformationException("Invalid PinCode");
+            }
+        }catch (InvalidUserInformationException e){
+            System.out.println(e);
+        }
+    }
+    public static boolean isValidInput(String regex, String userInput){
+        return Pattern.matches(regex,userInput);
+    }
+}
+
+class InvalidUserInformationException extends RuntimeException {
+    public InvalidUserInformationException() {
+        super();
+    }
+
+    public InvalidUserInformationException(String msg) {
+        super(msg);
+    }
+}
