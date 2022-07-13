e6eee3f4ab97a6ce5d56be39675848cf44b59c62
sandeepvarma8466 <svarma852@gmail.com>
2022-07-13 21:41:33 +0530

    Pincode UC4

--- src/main/java/com/blz/regex/pincode/PinCode.java
+++ src/main/java/com/blz/regex/pincode/PinCode.java
@@ -5,7 +5,7 @@ import java.util.regex.Pattern;
 
 public class PinCode {
     public static final Scanner sc = new Scanner(System.in);
-    public static final String PINCODE = "^[0-9]{6}$";
+    public static final String PINCODE = "^[0-9]{3}\\s{1}[0-9]{3}$";
     public static void main(String[] args) {
         PinCode pinCode = new PinCode();
         System.out.println("Enter pincode : ");
